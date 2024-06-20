c671c0d940f547cddf0819b28cf729e2c0a0bd0a
Dang Long <lilbua0710@gmail.com>
2024-06-21 00:43:03 +0700

    Chương trình in thông tin cá nhân

---
+++ Chương trình in thông tin cá nhân.py
@@ -0,0 +1,14 @@
+def display_information():
+    ho_va_ten = "Dang Ba Long"
+    ngay_sinh = "07/10/2000"
+    dia_chi = "Quận Nam Từ Liêm, Thành phố Hà Nội, Việt Nam"
+    muc_dich = "Học Python để phát triển kỹ năng lập trình."
+
+    print("Thông tin cá nhân của người học:")
+    print(f"Họ và tên: {ho_va_ten}")
+    print(f"Ngày sinh: {ngay_sinh}")
+    print(f"Địa chỉ: {dia_chi}")
+    print(f"Mục đích đến với khoá học Python: {muc_dich}")
+
+if __name__ == "__main__":
+    display_information()
